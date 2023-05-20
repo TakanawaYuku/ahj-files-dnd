3cedc2c2deb9175d92924fd7f75a3b446b025418
Takanawa Yuku <yukutakanawa@gmail.com>
2023-05-20 12:04:49 +0300

    edit code

--- src/js/app.js
+++ src/js/app.js
@@ -1,113 +1,30 @@
-import getRandomId from "../js/getRandomId";
-import updateLocalStorage from "../js/updateLocalStorage";
-import restoreBoardFromLocalStorage from "../js/restoreBoardFromLocalStorage";
-import insertAboveTask from "./insertAboveTask";
+import List from './list';
+import DnD from './dnd';
 
-window.onload = function () {
-  restoreBoardFromLocalStorage();
-};
+const containers = document.querySelectorAll('.list-container');
 
-const columnsContainer = document.querySelector(".board");
+containers.forEach((container) => {
+  const list = new List(container);
+  list.bindToDOM();
+});
 
-columnsContainer.addEventListener("click", (e) => {
-  //e.target - это конкретно тот элемент, на который мы кликнули
-  const clickTarget = e.target;
-  //console.log(clickTarget);
+const board = document.querySelector('.board');
+const hoverElements = document.querySelectorAll('.hover');
+const cardsVerticalDistance = 8;
+const dragAndDrop = new DnD(board, hoverElements, cardsVerticalDistance);
 
-  //!добавление новой карточки
-  const column = clickTarget.closest(".column");
-  const addCardLink = column.querySelector(".add-card-link");
-  const columnCardsList = column.querySelector(".column-cards");
-  const addCardSection = column.querySelector(".add-card-section");
-  const textarea = column.querySelector(".textarea");
-  const addCardButton = column.querySelector(".add-card-button");
-  const cancelCardButton = column.querySelector(".cancel-card-button");
+dragAndDrop.handleMousedown();
+dragAndDrop.handleMousemove();
+dragAndDrop.handleMouseleave();
+dragAndDrop.handleMouseup();
 
-  if (clickTarget === addCardLink) {
-    addCardSection.classList.remove("visually-hidden");
-
-    addCardButton.addEventListener("click", () => {
-      const cardContent = textarea.value.trim();
-
-      if (cardContent) {
-        const cardId = getRandomId();
-        const cardTemplate = `
-            <div class="card" draggable="true" data-card-id="${cardId}">
-            <button type="button" class="delete-card-button">&#x2715;</button>
-              <p class="card-text">${cardContent}</p>
-            </div>
-          `;
-
-        columnCardsList.insertAdjacentHTML("beforeend", cardTemplate);
-
-        updateLocalStorage();
-
-        textarea.value = "";
-        addCardSection.classList.add("visually-hidden");
-      }
-    });
-
-    cancelCardButton.addEventListener("click", () => {
-      textarea.value = "";
-      addCardSection.classList.add("visually-hidden");
-    });
+document.addEventListener('keyup', (event) => {
+  if (event.key === 'Enter') {
+    const cardsList = document.activeElement.closest('.list-cards');
+    const addBtn = cardsList.querySelector('.new-card-button');
+    const closeBtn = addBtn.nextElementSibling;
+    addBtn.click();
+    closeBtn.click();
+    document.activeElement.value = '';
   }
-
-  //!удаление карточки
-  const deleteCardButtons = column.querySelectorAll(".delete-card-button");
-
-  deleteCardButtons.forEach((button) => {
-    if (clickTarget === button) {
-      const card = button.closest(".card");
-      card.remove();
-
-      updateLocalStorage();
-    }
-  });
-});
-
-//!перетаскивание карточек
-let actualElement;
-
-const body = document.querySelector("body");
-
-columnsContainer.addEventListener("dragstart", (e) => {
-  actualElement = e.target;
-  //console.log(actualElement);
-
-  actualElement.classList.add("is-dragging");
-
-  // Устанавливаем курсор в режим "grabbing" во время перетаскивания
-  //actualElement.style.cursor = "grabbing";
-  //body.style.cursor = "grabbing";
-
-  const droppables = document.querySelectorAll(".column-cards");
-  //console.log(droppables);
-
-  droppables.forEach((zone) => {
-    zone.addEventListener("dragover", (e) => {
-      //console.log(e);
-      e.preventDefault();
-
-      const bottomCard = insertAboveTask(zone, e.clientY);
-      //console.log(bottomCard);
-      const curCard = document.querySelector(".is-dragging");
-
-      if (!bottomCard) {
-        zone.appendChild(curCard);
-      } else {
-        zone.insertBefore(curCard, bottomCard);
-      }
-
-      updateLocalStorage();
-    });
-  });
-});
-
-columnsContainer.addEventListener("dragend", () => {
-  actualElement.classList.remove("is-dragging");
-
-  // Возвращаем курсор в режим "grab" после окончания перетаскивания
-  //actualElement.style.cursor = "grab";
-  //body.style.cursor = "grab";
 });
